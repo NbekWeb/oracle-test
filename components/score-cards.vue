b6f9c8d4485344d9b7585f75322de7798826c45e
NbekWeb <nbekdev@gmail.com>
2025-03-24 03:18:33 +0500

    add oracle page

---
+++ components/score-cards.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="score-cards">
+    <div class="emotions-slider">
+      <div ref="swiper" class="swiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide" v-for="num in 10" :key="num">
+            <img src="@/assets/png/score.png" />
+            <h4>Sparkle monkey</h4>
+            <span> Flashing your shine, swinging to the top. </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import Swiper, { Autoplay } from 'swiper'
+import 'swiper/swiper-bundle.min.css'
+
+Swiper.use([Autoplay])
+
+@Component
+export default class ScoreCards extends Vue {
+  mounted() {
+    const swiperInstance = new Swiper(this.$refs.swiper as HTMLElement, {
+      slidesPerView: 'auto',
+      spaceBetween: 40,
+      loop: true,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      on: {
+        slideChange: function () {
+          document.querySelectorAll('.swiper-slide').forEach((slide) => {
+            ;(slide as HTMLElement).style.transform = 'scale(1)'
+            ;(slide as HTMLElement).style.zIndex = '1'
+          })
+
+          const activeSlide = this.slides[this.activeIndex]
+          if (activeSlide) {
+            ;(activeSlide as HTMLElement).style.transform = 'scale(1.3)'
+            ;(activeSlide as HTMLElement).style.zIndex = '10'
+          }
+        },
+      },
+    })
+
+    swiperInstance.emit('slideChange')
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.score-cards {
+  font-family: 'Kenyan Coffee';
+  color: #fff;
+  overflow-x: hidden;
+  padding: 50px 0;
+}
+
+.emotions-slider {
+  position: relative;
+  margin: auto;
+  max-width: 100%;
+}
+
+.swiper {
+  width: 100%;
+  overflow: visible;
+}
+
+.swiper-wrapper {
+  display: flex;
+}
+
+.swiper-slide {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  align-items: center;
+  color: #fff;
+  font-size: 24px;
+  width: 150px;
+  height: auto;
+  border-radius: 12px;
+  transition: transform 0.3s ease-in-out;
+  font-family: 'Kenyan Coffee';
+  text-align: center;
+  img {
+    width: 100%;
+    height: auto;
+  }
+  h4 {
+    font-size: 20px;
+    font-weight: 700;
+    margin-top: 8px;
+  }
+  span {
+    font-size: 12px;
+  }
+}
+</style>
---
+++ pages/score.vue
@@ -0,0 +1,217 @@
+<template>
+  <div class="score-page">
+    <div class="go-back">
+      <img :src="getImagePath('arrow-back')" />
+
+      <h3 class="main-title">Level</h3>
+    </div>
+    <banner-cards />
+    <div class="score-wrap">
+      <h3>score: 595</h3>
+      <score-cards />
+    </div>
+    <div class="score-rate">
+      <div class="rate-nums">
+        <span>450</span>
+        <span>650</span>
+      </div>
+      <div class="slide">
+        <span></span>
+      </div>
+      <span class="current">595</span>
+    </div>
+    <div class="rewards-wrap">
+      <h3>Your Status Rewards</h3>
+      <div class="rewards-card">
+        <div v-for="i in 3" :key="i">
+          <span></span>
+          Lorem ipsum dolor sit amet consectetur
+        </div>
+      </div>
+    </div>
+    <div class="overview-wrap">
+      <h3>Score overview</h3>
+      <div class="overview-cards">
+        <div v-for="(overview, i) of overviews" :key="i">
+          <span
+            class="status"
+            :class="overview.status == 'warn' ? 'warn' : 'success'"
+          >
+            {{ overview.title }}
+          </span>
+          <span>24.05.24, 05:20 PM </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({
+  components: {},
+})
+export default class ScorePage extends Vue {
+  getImagePath(icon: string) {
+    return require(`@/assets/svg/${icon}.svg`)
+  }
+
+  overviews = [
+    {
+      title: '+10 Succesfull deal',
+      status: 'success',
+    },
+    {
+      title: '+5 good review',
+      status: 'success',
+    },
+    {
+      title: '-5 Dispute lost',
+      status: 'warn',
+    },
+    {
+      title: '+10 succesfull deal',
+      status: 'success',
+    },
+    {
+      title: '+5 good review',
+      status: 'success',
+    },
+    {
+      title: '-5 Dispute lost',
+      status: 'warn',
+    },
+  ]
+
+  layout() {
+    return 'mobile'
+  }
+}
+</script>
+<style scoped lang="scss">
+.score-page {
+  font-family: 'Hanson';
+  color: #fff;
+  font-size: 14px;
+  h3 {
+    font-weight: 700;
+    text-transform: uppercase;
+  }
+  .go-back {
+    margin: 16px 0;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    img {
+      width: auto;
+      height: 20px;
+    }
+    h3 {
+      font-size: 16px;
+    }
+  }
+  .score-wrap {
+    h3 {
+      font-size: 20px;
+      text-align: center;
+      margin: 36px 0 20px 0;
+    }
+  }
+  .score-rate {
+    margin-bottom: 28px;
+    font-family: 'Reza Zulmi Alfaizi Sans';
+
+    .rate-nums {
+      display: flex;
+      justify-content: space-between;
+
+      color: #ffffff66;
+      margin-bottom: 8px;
+    }
+    .slide {
+      width: 100%;
+      height: 6px;
+      display: flex;
+      background: #181720;
+      border-radius: 6px;
+      position: relative;
+      span {
+        width: 70%;
+        position: absolute;
+        height: 100%;
+        background: #f64e2a;
+        border-radius: 6px;
+      }
+    }
+    .current {
+      display: flex;
+      margin-left: calc(70% - 15px);
+      margin-top: 6px;
+    }
+  }
+  .rewards-wrap {
+    margin-bottom: 36px;
+
+    h3 {
+      font-weight: 700;
+      font-size: 20px;
+      margin-bottom: 16px;
+    }
+    .rewards-card {
+      font-family: 'Reza Zulmi Alfaizi Sans';
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+      div {
+        width: 100%;
+        padding: 12px;
+        background: #181720;
+        border-radius: 8px;
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        span {
+          width: 8px;
+          height: 8px;
+          border-radius: 50%;
+          display: flex;
+          background: #f64e2a;
+        }
+      }
+    }
+  }
+  .overview-wrap {
+    h3 {
+      font-size: 20px;
+      font-weight: 700;
+      margin-bottom: 16px;
+    }
+    .overview-cards {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      div {
+        background: #1d1d2966;
+        border-radius: 12px;
+        padding: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-family: 'Reza Zulmi Alfaizi Sans';
+      }
+      .status {
+        font-family: 'Kenyan Coffee';
+        font-size: 20px;
+        font-weight: 700;
+        text-transform: uppercase;
+      }
+      .success {
+        color: #1b961f;
+      }
+      .warn {
+        color: #f64e2a;
+      }
+    }
+  }
+}
+</style>
